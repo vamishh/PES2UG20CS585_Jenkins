af64f2052250cc2ba5b829fb89787f41f00f3adb
vamishh <97085410+vamishh@users.noreply.github.com>
2023-02-16 11:32:12 +0530

    Update hello.cpp

--- Jenkins_lab-main/main/hello.cpp
+++ Jenkins_lab-main/main/hello.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
+  cout << "Hello, World from Vamshi\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
   cout << "This is the updated c++ file\n";
